6b44ee094c59bebe3bf88ff631fc26ec7b03d6f2
Noah Zoschke <noah@mixable.net>
2025-04-06 11:43:03 -0700

    unused

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,14 +1,4 @@
 import { plugin } from "bun";
 import { SveltePlugin } from "bun-plugin-svelte";
-import { afterEach, beforeEach } from "bun:test";
-import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
-beforeEach(async () => {
-  await GlobalRegistrator.register();
-});
-
-afterEach(async () => {
-  await GlobalRegistrator.unregister();
-});
-
-plugin(SveltePlugin({ forceSide: "client" }));
+plugin(SveltePlugin());
