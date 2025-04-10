bbb369b32a98ff1a52fd097ccb6dbe0a6dfce640
Noah Zoschke <noah@mixable.net>
2025-04-09 18:01:07 -0700

    home

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -79,7 +79,6 @@
           <h1 class="text-5xl font-bold">See it in action</h1>
           <p class="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
           <button class="btn btn-primary">Get Started</button>
