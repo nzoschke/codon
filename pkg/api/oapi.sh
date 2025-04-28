ebc6db75b2ec9df8a954551ece07d820ae21173c
Noah Zoschke <noah@mixable.net>
2025-04-28 08:37:40 -0700

    dont gen go sdk

--- pkg/api/oapi.sh
+++ pkg/api/oapi.sh
@@ -2,6 +2,4 @@
 
 cd ../..
 go run cmd/app/main.go openapi
-mkdir -p pkg/sdk
-go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest -generate "types,client" -package sdk doc/openapi.json | tail -n +2 > pkg/sdk/sdk.go
 bunx openapi-typescript doc/openapi.json --alphabetize -o src/schema.d.ts
